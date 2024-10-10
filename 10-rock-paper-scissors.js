13cdd3a957a0e406e43b98e5d50a5974813a6af4
SylvieMendez <137234443+SylvieMendez@users.noreply.github.com>
2024-10-10 14:07:53 -0400

    10-rock-paper-scissors.js

--- 10-rock-paper-scissors.js
+++ 10-rock-paper-scissors.js
@@ -63,8 +63,8 @@ document.querySelector('.js-result')
 .innerHTML = result;
 
 document.querySelector('.js-moves').innerHTML = 
-    `You <img src="images/${playerMove}-emoji.png" class="move-icon"> 
-    <img src="images/${computerMove}-emoji.png" class="move-icon"> Computer`;
+    `You <img src="${playerMove}-emoji.png" class="move-icon"> 
+    <img src="${computerMove}-emoji.png" class="move-icon"> Computer`;
 }
 
 function updateScoreElement () {
